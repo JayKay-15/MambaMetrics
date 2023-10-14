f78a9efde51488fc002e694c48019d7cf54e7b74
Jesse <jesse.kartes@gmail.com>
2023-10-14 14:18:05 -0500

    Update nba_scraper.R

--- code/nba_scraper.R
+++ code/nba_scraper.R
@@ -24,7 +24,11 @@ headers = c(
 # )
 
 
-sched <- nbastatR::game_logs(seasons = 2023, result_types = "team", season_types = "Regular Season") %>% select(5) %>% distinct()
+sched <- nbastatR::game_logs(seasons = 2023,
+                             result_types = "team",
+                             season_types = "Regular Season") %>%
+    select(5) %>%
+    distinct()
 
 # test <- seq(as.Date("2021-10-18"), as.Date("2022-10-19"), by="days")
 gm_df <- format(sched$dateGame, "%m/%d/%Y")
@@ -181,13 +185,113 @@ openxlsx::write.xlsx(dt, file = "./output/isolation_off.xlsx")
 
 
 
-
-
-
 # json <-
 #     res$content %>%
 #     rawToChar() %>%
 #     jsonlite::fromJSON(simplifyVector = T)
+
+
+
+
+# pie
+
+headers = c(
+    `Sec-Fetch-Site` = "same-site",
+    `Accept` = "*/*",
+    `Origin` = "https://www.nba.com",
+    `Sec-Fetch-Dest` = "empty",
+    `Accept-Language` = "en-US,en;q=0.9",
+    `Sec-Fetch-Mode` = "cors",
+    `Host` = "stats.nba.com",
+    `User-Agent` = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
+    `Referer` = "https://www.nba.com/",
+    `Accept-Encoding` = "gzip, deflate, br",
+    `Connection` = "keep-alive"
+)
+
+params = list(
+    `DateFrom` = "",
+    `DateTo` = "",
+    `GameSegment` = "",
+    `ISTRound` = "",
+    `LastNGames` = "0",
+    `LeagueID` = "00",
+    `Location` = "",
+    `MeasureType` = "Advanced",
+    `Month` = "0",
+    `OpponentTeamID` = "0",
+    `Outcome` = "",
+    `PORound` = "0",
+    `PaceAdjust` = "N",
+    `PerMode` = "Totals",
+    `Period` = "0",
+    `PlusMinus` = "N",
+    `Rank` = "N",
+    `Season` = "2022-23",
+    `SeasonSegment` = "",
+    `SeasonType` = "Regular Season",
+    `ShotClockRange` = "",
+    `VsConference` = "",
+    `VsDivision` = ""
+)
+
+res <- httr::GET(url = "https://stats.nba.com/stats/playergamelogs", httr::add_headers(.headers=headers), query = params)
+data <- httr::content(res) %>% .[['resultSets']] %>% .[[1]]
+column_names <- data$headers %>% as.character()  
+dt <- rbindlist(data$rowSet) %>% setnames(column_names)
+
+
+
+# pts off to, 2nd pts, fb pts, pitp
+
+headers = c(
+    `Sec-Fetch-Site` = "same-site",
+    `Accept` = "*/*",
+    `Origin` = "https://www.nba.com",
+    `Sec-Fetch-Dest` = "empty",
+    `Accept-Language` = "en-US,en;q=0.9",
+    `Sec-Fetch-Mode` = "cors",
+    `Host` = "stats.nba.com",
+    `User-Agent` = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
+    `Referer` = "https://www.nba.com/",
+    `Accept-Encoding` = "gzip, deflate, br",
+    `Connection` = "keep-alive"
+)
+
+params = list(
+    `DateFrom` = "",
+    `DateTo` = "",
+    `GameSegment` = "",
+    `ISTRound` = "",
+    `LastNGames` = "0",
+    `LeagueID` = "00",
+    `Location` = "",
+    `MeasureType` = "Misc",
+    `Month` = "0",
+    `OpponentTeamID` = "0",
+    `Outcome` = "",
+    `PORound` = "0",
+    `PaceAdjust` = "N",
+    `PerMode` = "Totals",
+    `Period` = "0",
+    `PlusMinus` = "N",
+    `Rank` = "N",
+    `Season` = "2022-23",
+    `SeasonSegment` = "",
+    `SeasonType` = "Regular Season",
+    `ShotClockRange` = "",
+    `VsConference` = "",
+    `VsDivision` = ""
+)
+
+res <- httr::GET(url = "https://stats.nba.com/stats/playergamelogs", httr::add_headers(.headers=headers), query = params)
+data <- httr::content(res) %>% .[['resultSets']] %>% .[[1]]
+column_names <- data$headers %>% as.character()  
+dt <- rbindlist(data$rowSet) %>% setnames(column_names)
+
+
+
+
 
 
 
